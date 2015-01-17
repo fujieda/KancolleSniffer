906c3f9121432dbe30bd9affe8339ecf26251ff3
Kazuhiro Fujieda <fujieda@users.sourceforge.jp>
2015-01-17 12:04:09 +0900

    バケツを使った入渠で消費した資材を資材の増減に反映させる

--- KancolleSniffer/DockInfo.cs
+++ KancolleSniffer/DockInfo.cs
@@ -48,27 +48,22 @@ namespace KancolleSniffer
                 _ndocTimers[id].SetEndTime(entry.api_complete_time);
                 var prev = _ndoc[id];
                 _ndoc[id] = (int)entry.api_ship_id;
-                if (prev == 0 && _ndoc[id] != 0) // 新たな入渠なら資源を減らす
-                {
-                    var history = _itemInfo.MaterialHistory;
-                    history[0].Now -= (int)entry.api_item1;
-                    history[1].Now -= (int)entry.api_item2;
-                    history[2].Now -= (int)entry.api_item3;
-                    history[3].Now -= (int)entry.api_item4;
-                }
-                else if (prev != 0 && _ndoc[id] == 0) // 修復完了
-                {
+                if (prev != 0 && _ndoc[id] == 0) // 修復完了
                     _shipInfo.RepairShip(prev);
-                }
             }
         }
 
         public void InspectNyukyo(string request)
         {
             var values = HttpUtility.ParseQueryString(request);
+            var id = int.Parse(values["api_ship_id"]);
+            int fuel, steal;
+            _shipInfo[id].CalcMaterialsToRepair(out fuel, out steal);
+            _itemInfo.MaterialHistory[(int)Material.Fuel].Now -= fuel;
+            _itemInfo.MaterialHistory[(int)Material.Steal].Now -= steal;
             if (int.Parse(values["api_highspeed"]) == 0)
                 return;
-            _shipInfo.RepairShip(int.Parse(values["api_ship_id"]));
+            _shipInfo.RepairShip(id);
             _itemInfo.MaterialHistory[(int)Material.Bucket].Now--;
         }
 
