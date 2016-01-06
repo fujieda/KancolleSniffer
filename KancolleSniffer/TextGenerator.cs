369a75c784c40b8e85a668103657c801e4707860
Kazuhiro Fujieda <fujieda@users.osdn.me>
2016-01-06 21:10:49 +0900

    クリップボードにコピーする際に制空値の上限と下限が同じなら範囲を示さない

--- KancolleSniffer/TextGenerator.cs
+++ KancolleSniffer/TextGenerator.cs
@@ -64,7 +64,8 @@ namespace KancolleSniffer
                     sb.Append("\r\n");
                 }
                 var fp = sniffer.GetFighterPower(f);
-                sb.Append($"制空: {fp[0]}～{fp[1]} 索敵: {sniffer.GetFleetLineOfSights(f):F1}\r\n");
+                sb.Append($"制空: {(fp[0] == fp[1] ? fp[0].ToString() : fp[0] + "～" + fp[1])} " +
+                          $"索敵: {sniffer.GetFleetLineOfSights(f):F1}\r\n");
             }
             return sb.ToString();
         }
