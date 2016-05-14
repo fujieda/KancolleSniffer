9e1787c683b3597b3aba492fbe35528d1011e00a
Kazuhiro Fujieda <fujieda@users.osdn.me>
2016-05-15 00:28:15 +0900

    バージョン8.10の準備

--- KancolleSniffer/Properties/AssemblyInfo.cs
+++ KancolleSniffer/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("KancolleSniffer")]
-[assembly: AssemblyCopyright("Copyright ©  Kazuhiro Fujieda 2013-2015")]
+[assembly: AssemblyCopyright("Copyright ©  Kazuhiro Fujieda 2013-2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // すべての値を指定するか、下のように '*' を使ってビルドおよびリビジョン番号を 
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("8.9.0.0")]
-[assembly: AssemblyFileVersion("8.9.0.0")]
+[assembly: AssemblyVersion("8.10.0.0")]
+[assembly: AssemblyFileVersion("8.10.0.0")]
