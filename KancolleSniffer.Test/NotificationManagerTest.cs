b2b89715404d915d7e0009806699c7867e0ffa42
Kazuhiro Fujieda <fujieda@users.osdn.me>
2017-11-03 19:00:09 +0900

    NotificationManagerのリファクタリング

---
+++ KancolleSniffer.Test/NotificationManagerTest.cs
@@ -0,0 +1,130 @@
+﻿// Copyright (C) 2017 Kazuhiro Fujieda <fujieda@users.osdn.me>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using ExpressionToCodeLib;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace KancolleSniffer.Test
+{
+    [TestClass]
+    public class NotificationManagerTest
+    {
+        private class MockTimer : NotificationManager.ITimer
+        {
+            private int _elapsed;
+            private bool _enabled;
+
+            public int Interval { get; set; }
+
+            public bool Enabled
+            {
+                get => _enabled;
+                set
+                {
+                    _enabled = value;
+                    _elapsed = 0;
+                }
+            }
+
+            public event EventHandler Tick;
+
+            public void Start()
+            {
+                Enabled = true;
+            }
+
+            public void Stop()
+            {
+                Enabled = false;
+            }
+
+            public void ElapseTime(int millis)
+            {
+                if (!Enabled)
+                    return;
+                var after = _elapsed + millis;
+                for (var n = _elapsed / Interval; n < after / Interval; n++)
+                {
+                    Tick?.Invoke(this, EventArgs.Empty);
+                }
+                _elapsed = after;
+            }
+        }
+
+        private class Message
+        {
+            public string Title { private get; set; }
+            public string Body { private get; set; }
+            public string Name { private get; set; }
+
+            public bool Equals(Message other) =>
+                other != null && Title == other.Title && Body == other.Body && Name == other.Name;
+        }
+
+        /// <summary>
+        /// 単発
+        /// </summary>
+        [TestMethod]
+        public void SingleNotification()
+        {
+            var timer = new MockTimer();
+            Message result = null;
+            var manager =
+                new NotificationManager((t, b, n) => { result = new Message {Title = t, Body = b, Name = n}; }, timer);
+            manager.Enqueue("遠征終了", "防空射撃演習");
+            PAssert.That(() => new Message {Title = "遠征が終わりました", Body = "防空射撃演習", Name = "遠征終了"}.Equals(result));
+        }
+
+        /// <summary>
+        /// 連続した通知の間隔を二秒空ける
+        /// </summary>
+        [TestMethod]
+        public void TwoNotificationAtSameTime()
+        {
+            var timer = new MockTimer();
+            Message result = null;
+            var manager =
+                new NotificationManager((t, b, n) => { result = new Message {Title = t, Body = b, Name = n}; }, timer);
+            manager.Enqueue("疲労回復40", 0, "cond40");
+            manager.Enqueue("疲労回復49", 1, "cond49");
+            PAssert.That(() => new Message {Title = "疲労が回復しました", Body = "第一艦隊 残り9分", Name = "疲労回復"}.Equals(result));
+            result = null;
+            timer.ElapseTime(1000);
+            PAssert.That(() => result == null);
+            timer.ElapseTime(1000);
+            PAssert.That(() => new Message {Title = "疲労が回復しました", Body = "第二艦隊", Name = "疲労回復"}.Equals(result));
+            timer.ElapseTime(2000);
+            PAssert.That(() => !timer.Enabled);
+        }
+
+        /// <summary>
+        /// 一つ目の通知の一秒後に投入された通知は一秒ずらす
+        /// </summary>
+        [TestMethod]
+        public void TwoNotification1SecDelay()
+        {
+            var timer = new MockTimer();
+            Message result = null;
+            var manager =
+                new NotificationManager((t, b, n) => { result = new Message {Title = t, Body = b, Name = n}; }, timer);
+            manager.Enqueue("建造完了", "第一ドック");
+            PAssert.That(() => new Message {Title = "建造が終わりました", Body = "第一ドック", Name = "建造完了"}.Equals(result));
+            timer.ElapseTime(1000);
+            manager.Enqueue("建造完了", "第二ドック");
+            timer.ElapseTime(1000);
+            PAssert.That(() => new Message {Title = "建造が終わりました", Body = "第二ドック", Name = "建造完了"}.Equals(result));
+        }
+    }
+}
--- KancolleSniffer/NotificationManager.cs
+++ KancolleSniffer/NotificationManager.cs
@@ -22,29 +22,27 @@ namespace KancolleSniffer
     public class NotificationManager
     {
         private readonly NotificationQueue _notificationQueue;
-        private readonly NotificationConfig _notificationConfig = new NotificationConfig();
 
-        public class Notification
+        private class Notification
         {
-            public string Title;
-            public string Body;
-            public string Name;
+            public string Key { get; set; }
+            public int Fleet { get; set; }
+            public string Subject { get; set; }
         }
 
-        public NotificationManager(Action<string, string, string> ring)
+        public NotificationManager(Action<string, string, string> ring, ITimer timer = null)
         {
-            _notificationQueue = new NotificationQueue(ring);
+
+            _notificationQueue = new NotificationQueue(ring, timer);
         }
 
         public void Enqueue(string key, int fleet, string subject)
         {
-            _notificationConfig.LoadConfig();
-            var notification = _notificationConfig[key];
             _notificationQueue.Enqueue(new Notification
             {
-                Title = ProcessFormatString(notification.Title, fleet, subject),
-                Body = ProcessFormatString(notification.Body, fleet, subject),
-                Name = notification.Name
+                Key = key,
+                Fleet = fleet,
+                Subject = subject
             });
         }
 
@@ -53,148 +51,101 @@ namespace KancolleSniffer
             Enqueue(key, 0, subject);
         }
 
-        private string ProcessFormatString(string format, int fleet, string subject)
-        {
-            var fn = new[] {"第一艦隊", "第二艦隊", "第三艦隊", "第四艦隊"};
-            var result = "";
-            var percent = false;
-            foreach (var ch in format)
-            {
-                if (ch == '%')
-                {
-                    if (percent)
-                    {
-                        percent = false;
-                        result += '%';
-                    }
-                    else
-                    {
-                        percent = true;
-                    }
-                }
-                else if (percent)
-                {
-                    percent = false;
-                    switch (ch)
-                    {
-                        case 'f':
-                            result += fn[fleet];
-                            break;
-                        case 's':
-                            result += subject;
-                            break;
-                        default:
-                            result += '%'.ToString() + ch;
-                            break;
-                    }
-                }
-                else
-                {
-                    result += ch;
-                }
-            }
-            return result;
-        }
-
         private class NotificationConfig
         {
-            private readonly Dictionary<string, Notification> _config = new Dictionary<string, Notification>();
+            public class Message
+            {
+                public string Title { get; set; }
+                public string Body { get; set; }
+                public string Name { get; set; }
+            }
 
-            private readonly Dictionary<string, Notification> _default = new Dictionary<string, Notification>
+            private readonly Dictionary<string, Message> _config = new Dictionary<string, Message>();
+
+            private readonly Dictionary<string, Message> _default = new Dictionary<string, Message>
             {
                 {
-                    "遠征終了", new Notification
+                    "遠征終了", new Message
                     {
                         Title = "遠征が終わりました",
-                        Body = "%s",
-                        Name = "遠征終了"
+                        Body = "%s"
                     }
                 },
                 {
-                    "入渠終了", new Notification
+                    "入渠終了", new Message
                     {
                         Title = "入渠が終わりました",
-                        Body = "%s",
-                        Name = "入渠終了"
+                        Body = "%s"
                     }
                 },
                 {
-                    "建造完了", new Notification
+                    "建造完了", new Message
                     {
                         Title = "建造が終わりました",
-                        Body = "%s",
-                        Name = "建造完了"
+                        Body = "%s"
                     }
                 },
                 {
-                    "艦娘数超過", new Notification
+                    "艦娘数超過", new Message
                     {
                         Title = "艦娘が多すぎます",
-                        Body = "%s",
-                        Name = "艦娘数超過"
+                        Body = "%s"
                     }
                 },
                 {
-                    "装備数超過", new Notification
+                    "装備数超過", new Message
                     {
                         Title = "装備が多すぎます",
-                        Body = "%s",
-                        Name = "装備数超過"
+                        Body = "%s"
                     }
                 },
                 {
-                    "大破警告", new Notification
+                    "大破警告", new Message
                     {
                         Title = "大破した艦娘がいます",
-                        Body = "%s",
-                        Name = "大破警告"
+                        Body = "%s"
                     }
                 },
                 {
-                    "泊地修理20分経過", new Notification
+                    "泊地修理20分経過", new Message
                     {
                         Title = "泊地修理 %f",
-                        Body = "20分経過しました。",
-                        Name = "泊地修理20分経過"
+                        Body = "20分経過しました。"
                     }
                 },
                 {
-                    "泊地修理進行", new Notification
+                    "泊地修理進行", new Message
                     {
                         Title = "泊地修理 %f",
-                        Body = "修理進行：%s",
-                        Name = "泊地修理進行"
+                        Body = "修理進行：%s"
                     }
                 },
                 {
-                    "泊地修理完了", new Notification
+                    "泊地修理完了", new Message
                     {
                         Title = "泊地修理 %f",
-                        Body = "修理完了：%s",
-                        Name = "泊地修理完了"
+                        Body = "修理完了：%s"
                     }
                 },
                 {
-                    "疲労回復40", new Notification
+                    "疲労回復40", new Message
                     {
                         Title = "疲労が回復しました",
-                        Body = "%f 残り9分",
-                        Name = "疲労回復"
+                        Body = "%f 残り9分"
                     }
                 },
                 {
-                    "疲労回復49", new Notification
+                    "疲労回復49", new Message
                     {
                         Title = "疲労が回復しました",
-                        Body = "%f",
-                        Name = "疲労回復"
+                        Body = "%f"
                     }
                 }
             };
 
-            public Notification this[string key] => _config.TryGetValue(key, out Notification value) ? value : _default[key];
+            public static string KeyToName(string key) => key.StartsWith("疲労回復") ? key.Substring(0, 4) : key;
 
-            public void LoadConfig()
+            private void LoadConfig()
             {
                 const string fileName = "notification.json";
                 _config.Clear();
@@ -205,11 +156,10 @@ namespace KancolleSniffer
                     {
                         if (!_default.ContainsKey(entry.key))
                             continue;
-                        _config[entry.key] = new Notification
+                        _config[entry.key] = new Message
                         {
                             Title = entry.title,
-                            Body = entry.body,
-                            Name = _default[entry.key].Name
+                            Body = entry.body
                         };
                     }
                 }
@@ -221,17 +171,111 @@ namespace KancolleSniffer
                     throw new Exception($"{fileName}に誤りがあります。: ${ex.Message}", ex);
                 }
             }
+
+            public Message GenerateMessage(Notification notification)
+            {
+                LoadConfig();
+                var format = _config.TryGetValue(notification.Key, out Message value) ? value : _default[notification.Key];
+                return new Message
+                {
+                    Title = ProcessFormatString(format.Title, notification.Fleet, notification.Subject),
+                    Body = ProcessFormatString(format.Body, notification.Fleet, notification.Subject),
+                    Name = KeyToName(notification.Key)
+                };
+            }
+
+            private string ProcessFormatString(string format, int fleet, string subject)
+            {
+                var fn = new[] {"第一艦隊", "第二艦隊", "第三艦隊", "第四艦隊"};
+                var result = "";
+                var percent = false;
+                foreach (var ch in format)
+                {
+                    if (ch == '%')
+                    {
+                        if (percent)
+                        {
+                            percent = false;
+                            result += '%';
+                        }
+                        else
+                        {
+                            percent = true;
+                        }
+                    }
+                    else if (percent)
+                    {
+                        percent = false;
+                        switch (ch)
+                        {
+                            case 'f':
+                                result += fn[fleet];
+                                break;
+                            case 's':
+                                result += subject;
+                                break;
+                            default:
+                                result += '%'.ToString() + ch;
+                                break;
+                        }
+                    }
+                    else
+                    {
+                        result += ch;
+                    }
+                }
+                return result;
+            }
+        }
+
+        public interface ITimer
+        {
+            int Interval { get; set; }
+            bool Enabled { get; set; }
+            event EventHandler Tick;
+            void Start();
+            void Stop();
+        }
+
+        public class TimerWrapper : ITimer
+        {
+            private readonly Timer _timer = new Timer();
+
+            public int Interval
+            {
+                get => _timer.Interval;
+                set => _timer.Interval = value;
+            }
+
+            public bool Enabled
+            {
+                get => _timer.Enabled;
+                set => _timer.Enabled = value;
+            }
+
+            public event EventHandler Tick
+            {
+                add => _timer.Tick += value;
+                remove => _timer.Tick -= value;
+            }
+
+            public void Start() => _timer.Start();
+
+            public void Stop() => _timer.Stop();
         }
 
         private class NotificationQueue
         {
             private readonly Action<string, string, string> _ring;
             private readonly Queue<Notification> _queue = new Queue<Notification>();
-            private readonly Timer _timer = new Timer {Interval = 2000};
+            private readonly ITimer _timer;
+            private readonly NotificationConfig _notificationConfig = new NotificationConfig();
 
-            public NotificationQueue(Action<string, string, string> ring)
+            public NotificationQueue(Action<string, string, string> ring, ITimer timer)
             {
                 _ring = ring;
+                _timer = timer ?? new TimerWrapper();
+                _timer.Interval = 2000;
                 _timer.Tick += TimerOnTick;
             }
 
@@ -243,7 +287,7 @@ namespace KancolleSniffer
                     return;
                 }
                 var notification = _queue.Dequeue();
-                _ring(notification.Title, notification.Body, notification.Name);
+                Ring(notification);
             }
 
             public void Enqueue(Notification notification)
@@ -254,9 +298,16 @@ namespace KancolleSniffer
                 }
                 else
                 {
-                    _ring(notification.Title, notification.Body, notification.Name);
+                    Ring(notification);
                     _timer.Start();
                 }
+            }
+
+            private void Ring(Notification notification)
+            {
+                var message =
+                    _notificationConfig.GenerateMessage(notification);
+                _ring(message.Title, message.Body, message.Name);
             }
         }
     }
