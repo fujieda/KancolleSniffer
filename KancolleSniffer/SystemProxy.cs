5ad4d7d81641e300cbe751f284096d64adc07ded
Kazuhiro Fujieda <fujieda@users.osdn.me>
2018-02-03 20:01:46 +0900

    プロキシの自動設定をバージョン9の方法に戻す

--- KancolleSniffer/SystemProxy.cs
+++ KancolleSniffer/SystemProxy.cs
@@ -14,75 +14,172 @@
 
 using System;
 using System.Runtime.InteropServices;
-using Microsoft.Win32;
-
-// ReSharper disable InconsistentNaming
+using FILETIME = System.Runtime.InteropServices.ComTypes.FILETIME;
 
 namespace KancolleSniffer
 {
-    public static class SystemProxy
+    public class SystemProxy
     {
-        private const string _regPath = @"Software\Microsoft\Windows\CurrentVersion\Internet Settings";
-        private const string _regName = "AutoConfigURL";
+        private InternetPerConnOptionList _orgList;
+        private string _currentUrl;
 
-        public static string AutoConfigUrl
+        private void SaveSettings()
         {
-            get
+            if (_orgList.dwSize != 0)
+                return;
+            var opts = new[]
             {
-                using (var regkey = Registry.CurrentUser.OpenSubKey(_regPath))
-                {
-                    if (regkey == null)
-                        return "";
-                    return regkey.GetValue(_regName) as string ?? "";
-                }
-            }
-            set
+                new InternetPerConnOption {dwOption = PerConnOption.INTERNET_PER_CONN_FLAGS},
+                new InternetPerConnOption {dwOption = PerConnOption.INTERNET_PER_CONN_AUTOCONFIG_URL}
+            };
+            var list = new InternetPerConnOptionList
             {
-                using (var regkey = Registry.CurrentUser.OpenSubKey(_regPath, true))
-                {
-                    if (regkey == null)
-                        return;
-                    if (value == "")
-                    {
-                        regkey.DeleteValue(_regName, false);
-                    }
-                    else
-                    {
-                        regkey.SetValue(_regName, value);
-                    }
-                }
-                Refresh();
+                pOptions = MarshalOptions(opts),
+                pszConnection = IntPtr.Zero,
+                dwOptionCount = opts.Length,
+                dwOptionError = 0
+            };
+            var listSize = list.dwSize = Marshal.SizeOf(list);
+            if (InternetQueryOption(IntPtr.Zero, InternetOption.INTERNET_OPTION_PER_CONNECTION_OPTION,
+                ref list, ref listSize))
+            {
+                _orgList = list;
             }
         }
 
-        /// <summary>
-        /// PACファイルでDIRECTを指定すると、すべてのサイトがローカルイントラネットになり、
-        /// IEが互換表示になるなどの不具合があるので、イントラネットにならないようにする
-        /// </summary>
-        public static void AdjustLocalIntranetZoneFlags()
+        public void SetAutoConfigUrl(string url)
         {
-            var zones = Registry.CurrentUser.OpenSubKey(_regPath + @"\Zones\1", true);
-            if (zones == null)
+            SaveSettings();
+            var flagValue = new InternetPerConnOptionValue {dwValue = (int)PerConnFlags.PROXY_TYPE_AUTO_PROXY_URL};
+            var urlValue = new InternetPerConnOptionValue {pszValue = Marshal.StringToHGlobalAuto(url)};
+            _currentUrl = url;
+            var opts = new[]
+            {
+                new InternetPerConnOption {dwOption = PerConnOption.INTERNET_PER_CONN_FLAGS, Value = flagValue},
+                new InternetPerConnOption {dwOption = PerConnOption.INTERNET_PER_CONN_AUTOCONFIG_URL, Value = urlValue}
+            };
+            var list = new InternetPerConnOptionList
+            {
+                pOptions = MarshalOptions(opts),
+                pszConnection = IntPtr.Zero,
+                dwOptionCount = opts.Length,
+                dwOptionError = 0
+            };
+            var listSize = list.dwSize = Marshal.SizeOf(list);
+            var listBuff = Marshal.AllocCoTaskMem(listSize);
+            Marshal.StructureToPtr(list, listBuff, false);
+            InternetSetOption(IntPtr.Zero, InternetOption.INTERNET_OPTION_PER_CONNECTION_OPTION, listBuff, listSize);
+            Refresh();
+
+            Marshal.FreeHGlobal(urlValue.pszValue);
+            Marshal.FreeCoTaskMem(list.pOptions);
+            Marshal.FreeCoTaskMem(listBuff);
+        }
+
+        public void RestoreSettings()
+        {
+            if (_orgList.dwSize == 0)
                 return;
-            if (!(zones.GetValue("Flags") is int flags))
-                return;
-            zones.SetValue("Flags", flags & (-1 ^ 0x108));
+            var size = Marshal.SizeOf(typeof(InternetPerConnOption));
+            var urlOpt = (InternetPerConnOption)
+                Marshal.PtrToStructure((IntPtr)((long)_orgList.pOptions + size), typeof(InternetPerConnOption));
+            var orgUrl = Marshal.PtrToStringUni(urlOpt.Value.pszValue);
+            if (orgUrl == _currentUrl) // The restoration was sikipped or failed at last time.
+            {
+                // Unselect the Use automatic configration script check box.
+                var flagsOpt =
+                    (InternetPerConnOption)Marshal.PtrToStructure(_orgList.pOptions, typeof(InternetPerConnOption));
+                flagsOpt.Value.dwValue &= ~(int)PerConnFlags.PROXY_TYPE_AUTO_PROXY_URL;
+                Marshal.StructureToPtr(flagsOpt, _orgList.pOptions, false);
+            }
+            var listSize = _orgList.dwSize;
+            var listBuff = Marshal.AllocCoTaskMem(listSize);
+            Marshal.StructureToPtr(_orgList, listBuff, false);
+            InternetSetOption(IntPtr.Zero, InternetOption.INTERNET_OPTION_PER_CONNECTION_OPTION, listBuff, listSize);
+            Refresh();
+
+            Marshal.FreeCoTaskMem(listBuff);
+            Marshal.FreeHGlobal(urlOpt.Value.pszValue);
+            Marshal.FreeCoTaskMem(_orgList.pOptions);
+            _orgList.dwSize = 0;
+        }
+
+        private IntPtr MarshalOptions(InternetPerConnOption[] opts)
+        {
+            var buff = Marshal.AllocCoTaskMem(Marshal.SizeOf(typeof(InternetPerConnOption)) * opts.Length);
+            var size = Marshal.SizeOf(typeof(InternetPerConnOption));
+            for (var i = 0; i < opts.Length; i++)
+            {
+                var ptr = (IntPtr)((long)buff + (i * size));
+                Marshal.StructureToPtr(opts[i], ptr, false);
+            }
+            return buff;
         }
 
         public static void Refresh()
         {
-            InternetSetOption(IntPtr.Zero, InternetOption.INTERNET_OPTION_SETTINGS_CHANGED, IntPtr.Zero, 0);
-            InternetSetOption(IntPtr.Zero, InternetOption.INTERNET_OPTION_REFRESH, IntPtr.Zero, 0);
+            InternetSetOption(IntPtr.Zero, InternetOption.INTERNET_OPTION_PROXY_SETTINGS_CHANGED, IntPtr.Zero, 0);
         }
 
-        [DllImport("WinInet.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        [DllImport("WinInet.dll", CharSet = CharSet.Unicode)]
+        private static extern bool InternetQueryOption(IntPtr hInternet, InternetOption dwOption,
+            ref InternetPerConnOptionList optionList, ref int lpdwBufferLength);
+
+        [DllImport("WinInet.dll", CharSet = CharSet.Unicode)]
         private static extern bool InternetSetOption(IntPtr hInternet, InternetOption dwOption,
             IntPtr lpBuffer, int dwBufferLength);
 
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
+        private struct InternetPerConnOptionList
+        {
+            public int dwSize;
+            public IntPtr pszConnection;
+            public int dwOptionCount;
+            public int dwOptionError;
+            public IntPtr pOptions;
+        }
+
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
+        private struct InternetPerConnOption
+        {
+            public PerConnOption dwOption;
+            public InternetPerConnOptionValue Value;
+        }
+
+        [StructLayout(LayoutKind.Explicit)]
+        public struct InternetPerConnOptionValue
+        {
+            [FieldOffset(0)] public int dwValue;
+            [FieldOffset(0)] public IntPtr pszValue;
+            [FieldOffset(0)] public FILETIME ftValue;
+        }
+
+// ReSharper disable UnusedMember.Local
+// ReSharper disable InconsistentNaming
+
         private enum InternetOption : uint
         {
-            INTERNET_OPTION_REFRESH = 37,
-            INTERNET_OPTION_SETTINGS_CHANGED = 39
+            INTERNET_OPTION_REFRESH = 0x00000025,
+            INTERNET_OPTION_PER_CONNECTION_OPTION = 0x0000004B,
+            INTERNET_OPTION_PROXY_SETTINGS_CHANGED = 0x0000005F
+        }
+
+        private enum PerConnOption
+        {
+            INTERNET_PER_CONN_FLAGS = 1,
+            INTERNET_PER_CONN_PROXY_SERVER = 2,
+            INTERNET_PER_CONN_PROXY_BYPASS = 3,
+            INTERNET_PER_CONN_AUTOCONFIG_URL = 4,
+            INTERNET_PER_CONN_AUTODISCOVERY_FLAGS = 5
+        }
+
+        [Flags]
+        private enum PerConnFlags
+        {
+            PROXY_TYPE_DIRECT = 0x00000001,
+            PROXY_TYPE_PROXY = 0x00000002,
+            PROXY_TYPE_AUTO_PROXY_URL = 0x00000004,
+            PROXY_TYPE_AUTO_DETECT = 0x00000008
         }
     }
 }
