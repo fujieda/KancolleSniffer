fb481494ae950cdee304f5e5423ba4d0114c4056
Kazuhiro Fujieda <fujieda@users.osdn.me>
2018-08-17 22:07:39 +0900

    バージョン11.3の準備

--- KancolleSniffer/Properties/AssemblyInfo.cs
+++ KancolleSniffer/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // すべての値を指定するか、下のように '*' を使ってビルドおよびリビジョン番号を 
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("11.2.0.0")]
-[assembly: AssemblyFileVersion("11.2.0.0")]
+[assembly: AssemblyVersion("11.3.0.0")]
+[assembly: AssemblyFileVersion("11.3.0.0")]
